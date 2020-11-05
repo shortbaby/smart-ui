ca38f28336c4a27b929283c78ff8ab52ffe247bd
shortbaby <smart25xy@qq.com>
2020-11-06 04:54:14 +0800

    feat(src/component/button): 添加button文件夹
    添加button组件
    
    BREAKING CHANGE: 新增button组件文件

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+/*
+ * @Author: bingwen
+ * @Date: 2020-11-06 04:52:55
+ * @LastEditors: bingwen
+ * @LastEditTime: 2020-11-06 04:53:03
+ * @Description: 
+ * @FilePath: /formily/Users/xuguanlong/workspace/smart-ui/src/components/Button/index.tsx
+ */
+const a = 1
