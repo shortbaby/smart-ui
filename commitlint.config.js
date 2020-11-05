0082c7112e6627fbb23b58be9efe9ad2263b0270
shortbaby <smart25xy@qq.com>
2020-11-06 04:20:41 +0800

    chore: modfiy eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-restricted-globals */
 /*
  * @Author: bingwen
  * @Date: 2020-11-06 03:49:39
  * @LastEditors: bingwen
- * @LastEditTime: 2020-11-06 03:49:51
+ * @LastEditTime: 2020-11-06 04:20:02
  * @Description: 
  * @FilePath: /formily/Users/xuguanlong/workspace/smart-ui/commitlint.config.js
  */
 module.exports = {
   extends: ['@commitlint/config-conventional']
-};
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: bingwen
+ * @Date: 2020-11-06 04:12:06
+ * @LastEditors: bingwen
+ * @LastEditTime: 2020-11-06 04:14:22
+ * @Description: 
+ * @FilePath: /formily/Users/xuguanlong/workspace/smart-ui/src/index.ts
+ */
+
+let a = 1
+
+export default a
