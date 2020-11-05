445b18c7979ad75f24d5029d2994eac4f490a77e
shortbaby <smart25xy@qq.com>
2020-11-06 03:53:53 +0800

    添加环境配置文件

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: bingwen
+ * @Date: 2020-11-06 03:49:39
+ * @LastEditors: bingwen
+ * @LastEditTime: 2020-11-06 03:49:51
+ * @Description: 
+ * @FilePath: /formily/Users/xuguanlong/workspace/smart-ui/commitlint.config.js
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+};
